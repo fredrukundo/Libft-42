d614096302e855792fc28d5df0aee2295af2913f
rukundo fred <105452272+fredrukundo@users.noreply.github.com>
2023-11-26 15:21:31 +0100

    added overflow protection

--- ft_calloc.c
+++ ft_calloc.c
@@ -15,10 +15,24 @@
 void	*ft_calloc(size_t num_elements, size_t size)
 {
 	void	*array;
-
+	// overflow protection for calloc
+	size_t fred;
+        fred = num_elements * size;
+        if (fred != 0 && fred / num_elements != size )
+        {
+          printf("error");
+          return (NULL);
+        }
 	array = (void *)malloc(num_elements * size);
 	if (array == NULL)
 		return (NULL);
 	ft_bzero(array, (num_elements * size));
 	return (array);
+}
+
+// testing
+int main()
+{
+ ft_calloc(10000000000,SIZE_T_MAX);
+ return 0;
 }
