8be12d2f8c1466190fa9841d528380b635978207
Fred Rukundo <dukefred9@gmail.com>
2024-10-26 05:44:55 +0100

    atoi overflow protection

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,44 +6,66 @@
 /*   By: frukundo <frukundo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 18:42:23 by frukundo          #+#    #+#             */
-/*   Updated: 2023/11/10 23:05:30 by frukundo         ###   ########.fr       */
+/*   Updated: 2024/10/26 05:44:05 by frukundo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	check_overflow(unsigned long int res, int sign)
+#include <stdio.h>
+
+
+static int	isoverflow(long long int check, long long int res)
 {
-	if (res > 9223372036854775807 && sign == 1)
-		res = -1;
-	else if (res > 9223372036854775807 && sign == -1)
-		res = 0;
-	return ((int)(res * sign));
+	if (res / 10 == check)
+		return (0);
+	return (1);
+}
+
+static int	is_sspace(const char *str, int *pt)
+{
+	int	i;
+	int	retsi;
+
+	i = 0;
+	retsi = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			retsi *= -1;
+		i++;
+	}
+	*pt = i;
+	return (retsi);
 }
 
 int	ft_atoi(const char *str)
 {
-	unsigned long int	res;
-	int					i;
-	int					sign;
+	long long int	r;
+	int				s;
+	long long int	check;
+	int				i;
 
-	i = 0;
-	sign = 1;
-	res = 0;
-	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	if (str[i] == '-')
+	r = 0;
+	s = is_sspace(str, &i);
+	while (str[i] >= '0' && str[i] <= '9')
 	{
-		sign = -1;
+		check = r;
+		r = r * 10 + (str[i] - '0');
+		if (isoverflow(check, r) == 1)
+		{
+			if (s == -1)
+				return (0);
+			return (-1);
+		}
 		i++;
 	}
-	else if (str[i] == '+')
-		i++;
-	while (ft_isdigit(str[i]))
-	{
-		res *= 10;
-		res += str[i] - '0';
-		i++;
-	}
-	return (check_overflow(res, sign));
+	return (r * s);
+}
+
+int main() {
+    printf("output: %d\n", ft_atoi("92233720368547758071119223372036854775807111"));
+    printf("output1: %d\n", atoi("92233720368547758071119223372036854775807111"));
 }
